3725f9a5f56c2b9b636d551acbd57297f38b977c
Aaron Contreras <aaronlcaq@gmail.com>
2020-06-25 18:34:57 -0500

    Use each_with_object

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,11 +1,16 @@
+# frozen_string_literal: true
+
 def stock_picker(list_of_prices)
-  profits = {}
-  list_of_prices.each_with_index do |price, day_number|
-    highest_in_range = list_of_prices[day_number..-1].max
+  list_of_prices.each_with_object({}).with_index do |(price, profits), buying_day|
+    remaining_days = list_of_prices[buying_day..-1]
+    highest_in_range = remaining_days.max
+    selling_day = buying_day + remaining_days.index(highest_in_range)
+
     profit = highest_in_range - price
-    profits[profit] = [day_number, list_of_prices.index(highest_in_range)]
-  end
-  profits.max
+
+    profits[profit] = [buying_day, selling_day]
+    profits
+  end.max_by(&:first)
 end
 
 def output_centered(text)
@@ -14,17 +19,18 @@ end
 
 ###
 
-output_centered "I'm that stock market black-box everyone so much desires" 
-output_centered "Give me a list of prices separated by commas for each day"
+output_centered "I'm that stock market black-box everyone so much desires"
+output_centered 'Give me a list of prices separated by commas for each day'
 output_centered "and I'll tell you what to do with your money..."
-list_of_prices = gets.chomp.gsub(/\s*/, '').split(',').map {|price| price.to_i}
+list_of_prices = gets.chomp.gsub(/\s*/, '').split(',').map(&:to_i)
+
 best_investment = stock_picker(list_of_prices)
 
 if best_investment.first.zero?
-  investment_advice = "There will be better times to invest... Keep your money safe and come back some other day." 
+  puts 'There will be better times to invest... Keep your money safe and come back some other day.'
 else
-  investment_advice = "The wisest investment opportunity is on day #{best_investment.last[0]}, followed by a sell on day #{best_investment.last[1]} for a profit of $#{best_investment.first}."
+  puts "The wisest investment opportunity is on day #{best_investment.last[0]},\
+  followed by a sell on day #{best_investment.last[1]} for a profit of $#{best_investment.first}."
 end
 
-puts investment_advice
-puts "Trust me *wink wink*"
+puts 'Trust me *wink wink*'
